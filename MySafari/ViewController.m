d6509068b95f58439c89a3f6ac9c5a6b0f7e4a17
Timothy Yeh <timothy.yeh@gmail.com>
2015-03-11 14:59:04 -0700

    init commit

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -17,11 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.webView.delegate = self;
 }
 
 @end
